c812be10088ac215558278428b46ebd70dba511c
Nghiemducnam <nghiemducnam0312@gmail.com>
2020-07-04 16:00:19 +0700

    push to repo

--- src/app/models/IrregularVerbSearch.ts
+++ src/app/models/IrregularVerbSearch.ts
@@ -1,11 +1,11 @@
-export interface IrregularVerb {
+import {BaseSearch} from './BaseSearch';
+
+export interface IrregularVerbSearch extends BaseSearch{
   id?: number;
   verb?: string;
   verb1?: string;
   verb2?: string;
   meaning?: string;
-  spellingVerb?: string;
-  spellingVerb1?: string;
-  spellingVerb2?: string;
+
   verbCategory?: number;
 }
